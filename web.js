0d45c3a679e229996d3366e070366502f43b581b
Simon <simon.grzebieta@innablr.com>
2017-05-11 14:30:21 +1000

    nodejs hello world

---
+++ web.js
@@ -0,0 +1,16 @@
+var http = require('http');
+//loads http module
+var app=http.createServer(function (req, res) {
+//creates server
+  res.writeHead(200, {'Content-Type': 'text/plain'});
+  //sets the right header and status code
+  res.end('Hello World\n');
+  //outputs string with line end symbol
+});
+var port = process.env.PORT || 5000;
+app.listen(port, function() {
+  console.log("Listening on " + port);
+});
+// }).listen(1337, "127.0.0.1");
+//sets port and IP address of the server
+console.log('Server running at http://127.0.0.1:1337/');
